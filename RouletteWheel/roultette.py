c2a15c7a774020cf196f9e3c814f2ea19e3964f6
Rick <Rick@WEMBLEY>
2017-02-06 03:55:48 -0500

    Add project files.

---
+++ RouletteWheel/roultette.py
@@ -0,0 +1,8 @@
+#Project: CIS 177 WEEK 1 PROJECT
+#Project Location: projects\cis177\Week3
+#File: first.py
+#Purpose: Request input of a number representing a pocket on a roulette wheel
+#         Then print the color of the chosen pocket number to standard output
+#Revision: 1.0 / 5 FEB 2017
+#Created: 5 FEB 2017
+#Author: Rick Miller <rick@rickthegeek.com>
